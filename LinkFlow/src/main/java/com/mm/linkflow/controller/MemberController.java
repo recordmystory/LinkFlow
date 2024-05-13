057281f3e383b00d2a3b54a82de5e44066adb40b
heekang93 <153481850+heekang93@users.noreply.github.com>
2024-05-13 17:29:00 +0900

    엄희강 로그아웃 기능 구현 완
    완료

--- LinkFlow/src/main/java/com/mm/linkflow/controller/MemberController.java
+++ LinkFlow/src/main/java/com/mm/linkflow/controller/MemberController.java
@@ -7,10 +7,12 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mm.linkflow.dto.MemberDto;
 import com.mm.linkflow.service.service.MemberService;
@@ -21,8 +23,15 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
- 
-
+	
+	//로그아웃
+	@RequestMapping("/loginout.me")
+	public String signout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	
 		
 }
 
