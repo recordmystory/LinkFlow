2f8b431d46e2c432abe2eb60c78cd9b31b473350
recordmystory <sdl13862@naver.com>
2024-05-14 13:40:26 +0900

    update EdsmProgServiceImpl
    - 메소드 추가

--- LinkFlow/src/main/java/com/mm/linkflow/service/impl/EdsmProgServiceImpl.java
+++ LinkFlow/src/main/java/com/mm/linkflow/service/impl/EdsmProgServiceImpl.java
@@ -1,5 +1,21 @@
 package com.mm.linkflow.service.impl;
 
-public interface EdsmProgServiceImpl {
+import java.util.List;
+
+import com.mm.linkflow.dto.EdocDto;
+import com.mm.linkflow.dto.PageInfoDto;
+import com.mm.linkflow.service.service.EdsmProgService;
+
+public class EdsmProgServiceImpl implements EdsmProgService {
+
+	@Override
+	public int selectAllListCount() {
+		return 0;
+	}
+
+	@Override
+	public List<EdocDto> selectAllList(PageInfoDto pi) {
+		return null;
+	}
 
 }
