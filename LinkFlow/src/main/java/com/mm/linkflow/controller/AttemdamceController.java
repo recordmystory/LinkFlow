549fdee4893b90cc044ffaf5606c2bf34a35216c
hwangminwoo <135596448+hwangminwoo4@users.noreply.github.com>
2024-05-17 01:37:29 +0900

    update : 출퇴근 관련 코드 추가
    update : 출퇴근 관련 코드 추가
    author : 황민우

--- LinkFlow/src/main/java/com/mm/linkflow/controller/AttemdamceController.java
+++ LinkFlow/src/main/java/com/mm/linkflow/controller/AttemdamceController.java
@@ -1,7 +1,11 @@
 package com.mm.linkflow.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mm.linkflow.service.service.AttemdamceService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +14,11 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class AttemdamceController {
 	
+	private final AttemdamceService attService;
 	
+	@ResponseBody
+	@PostMapping("/checkOut.do")
+	public String checkOutAtt(String userId) {
+		return attService.checkOutAtt(userId) > 0 ? "success" : "fail";
+	}
 }
