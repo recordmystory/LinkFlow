0a461afb7fcea7dbad5e6cfafa9aa2b3aa18c8ea
hwangminwoo <135596448+hwangminwoo4@users.noreply.github.com>
2024-05-16 23:14:17 +0900

    update : 근무체크 관련 코드 추가
    update : 근무체크 관련 코드 추가
    author : 황민우
    
    로그인 시 출근처리 되는 코드 추가 :
    - HomeController.java
    - AttemdamceDao.java
    - AttemdamceServiceIpml.java
    - AttemdamceService.java
    - attemdamce-mapper.xml

--- LinkFlow/src/main/java/com/mm/linkflow/dao/AttemdamceDao.java
+++ LinkFlow/src/main/java/com/mm/linkflow/dao/AttemdamceDao.java
@@ -11,4 +11,14 @@ public class AttemdamceDao {
 	private final SqlSessionTemplate sql;
 	
 	
+	// 출근 체크 확인용
+	public int selectCheckIn(String userId) {
+		return sql.selectOne("attemdamceDaoMapper.selectCheckIn", userId);
+	}
+	
+	// 출근 insert 용
+	public int checkInAtt(String userId) {
+		return sql.insert("attemdamceDaoMapper.checkInAtt", userId);
+	}
+	
 }
--- LinkFlow/src/main/java/com/mm/linkflow/service/service/AttemdamceService.java
+++ LinkFlow/src/main/java/com/mm/linkflow/service/service/AttemdamceService.java
@@ -6,4 +6,10 @@ public interface AttemdamceService {
 	
 	//나의 근태를 확인
 	AttemdamceDto myAttemdamce(AttemdamceDto att);
+	
+	// 출근 체크 확인용
+	int selectCheckIn(String userId);
+	
+	// 출근 insert 용
+	int checkInAtt(String userId);
 }
