afdef6f523136e83de6bc21b0cbdb318a4dae4e2
recordmystory <113417749+recordmystory@users.noreply.github.com>
2024-05-18 23:00:07 +0900

    feat : 서명 설정
    - 선택한 파일로 서명 설정
    - 마우스로 그려서 서명 설정
    - 기본 이미지로 서명 재설정
    
    기능구현 완료

--- LinkFlow/src/main/java/com/mm/linkflow/controller/EdsmSignController.java
+++ LinkFlow/src/main/java/com/mm/linkflow/controller/EdsmSignController.java
@@ -1,8 +1,23 @@
 package com.mm.linkflow.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.Base64;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mm.linkflow.dto.MemberDto;
+import com.mm.linkflow.service.service.EdsmSignService;
+import com.mm.linkflow.util.FileUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,7 +26,101 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EdsmSignController {
 	
-	@GetMapping("/setting.sign")
+	private final EdsmSignService edsmSignService;
+	private final FileUtil fileUtil;
+	// private final CommonsMultipartResolver multipartResolver;
+	
+	/** 기본 이미지로 서명 설정
+	 * 
+	 * @param uploadFile
+	 * @param session
+	 * @return SUCCESS | FAIL
+	 * 
+	 * @author 김지우
+	 */
+	@ResponseBody
+	@PostMapping("/modifySignDefault.sg")
+	public String modifySignDefault(MultipartFile uploadFile, HttpSession session, RedirectAttributes redirectAttributes) {
+		MemberDto loginUser = (MemberDto) session.getAttribute("loginUser");
+		String originalSignUrl = loginUser.getSignUrl();
+		
+		Map<String, String> map = fileUtil.fileUpload(uploadFile, "sign");
+		loginUser.setSignUrl(map.get("filePath") + "/" + map.get("filesystemName"));
+		
+		int result = edsmSignService.uploadSignImg(loginUser);
+		
+		if(result > 0) {
+			if(originalSignUrl != null) {
+				new File(originalSignUrl).delete();
+			}
+			return "SUCCESS";
+		} else {
+			new File(map.get("filePath") + "/" + map.get("filesystemName")).delete();
+			return "FAIL";
+		}
+	}
+	
+	/** 모달 내에서 마우스로 그려 서명 설정 (Canvas API)
+	 * 
+	 * @param uploadFile
+	 * @param session
+	 * @return SUCCESS | FAIL
+	 * 
+	 * @author 김지우
+	 */
+	@ResponseBody
+	@PostMapping("/modifySignCanvas.sg")
+	public String ajaxModifySignCanvas(MultipartFile uploadFile, HttpSession session) {
+		MemberDto loginUser = (MemberDto) session.getAttribute("loginUser");
+		String originalSignUrl = loginUser.getSignUrl();
+		
+		Map<String, String> map = fileUtil.fileUpload(uploadFile, "sign");
+		loginUser.setSignUrl(map.get("filePath") + "/" + map.get("filesystemName"));
+		
+		int result = edsmSignService.uploadSignImg(loginUser);
+		
+		if(result > 0) {
+			if(originalSignUrl != null) {
+				new File(originalSignUrl).delete();
+			}
+			return "SUCCESS";
+		} else {
+			new File(map.get("filePath") + "/" + map.get("filesystemName")).delete();
+			return "FAIL";
+		}
+	}
+	
+	/** 선택한 파일로 서명 설정
+	 * 
+	 * @param uploadFile
+	 * @param session
+	 * @return SUCCESS | FAIL
+	 * 
+	 * @author 김지우
+	 */
+	@ResponseBody
+	@PostMapping("/modifySignFile.sg")
+	public String ajaxModifySign(MultipartFile uploadFile, HttpSession session) {
+		MemberDto loginUser = (MemberDto) session.getAttribute("loginUser");
+		String originalSignUrl = loginUser.getSignUrl();
+		
+		Map<String, String> map = fileUtil.fileUpload(uploadFile, "sign");
+		loginUser.setSignUrl(map.get("filePath") + "/" + map.get("filesystemName"));
+		
+		int result = edsmSignService.uploadSignImg(loginUser);
+		
+		if(result > 0) {
+			if(originalSignUrl != null) {
+				new File(originalSignUrl).delete();
+			}
+			return "SUCCESS";
+		} else {
+			new File(map.get("filePath") + "/" + map.get("filesystemName")).delete();
+			return "FAIL";
+		}
+	}
+	
+	@GetMapping("/setting.sg")
 	public String listAll() {
 		return "/edsm/sign/setting";
 	}
